1db581da81a4de6ad68cd93d8ef474e5726e022a
Shahina Rahman <38435711+srahman-24@users.noreply.github.com>
2022-05-03 14:15:36 -0500

    Update GMac_bic.R

--- Codes/GMac_bic.R
+++ Codes/GMac_bic.R
@@ -2,6 +2,7 @@ GMac_bic = function(X, kmax){
   
   
   library(mclust)
+  
   N           = nrow(X)
   p           = ncol(X)
   GG          = tcrossprod(X, X)/p
@@ -9,17 +10,20 @@ GMac_bic = function(X, kmax){
   GG_new      = cbind(gg_wodiag + diag(colSums(gg_wodiag)/(N - 1)), diag(GG))
   Mclus       = mclust::Mclust(GG_new, modelNames = "VVI", verbose = F, G = 1:kmax)
   delta       = Mclus$classification
-  
-  ## M-delta update
-  for (i in 1:N)
-  {
-    index        = which(delta == delta[i])
-    GG_new[i, i] = sum(GG_new[index, i]) / length( index)
-  }
-  Gclust         = mclust::Mclust(GG_new, modelNames = "VVI", verbose = F, G = 1:kmax)
-  
+
+   Mdelta     = GG_new
+   ## M-delta update
+   for (i in 1:N)
+   {
+      index           = which(delta == delta[i])
+      Mdelta[i, i]    = (sum(GG_new[index, i]) - GG_new[i,i]) / (length(index) - 1)
+   }
+  Gclust            = mclust::Mclust(Mdelta, modelNames = "VVI", verbose = F, G = 1:kmax)
   return(list(class = Gclust$classification, K = Gclust$G, bic = Gclust$BIC))
   
 }
+
+
+
 
 
