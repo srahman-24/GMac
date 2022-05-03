4d1995d097c2a87aced1ac848849ecfea707c73d
Shahina Rahman <38435711+srahman-24@users.noreply.github.com>
2022-05-03 14:16:22 -0500

    Update main_Gmac.R

--- Codes/main_Gmac.R
+++ Codes/main_Gmac.R
@@ -14,15 +14,15 @@ source("AMI.R")
 folder      = "/Users/srahman/Desktop/paper/RJClust/desktop_codes/Microarray-data/"
 data_files  = list.files(path = folder, pattern = "*.csv")
 
-for (i in 1:length(data_files)) 
+for (i in 1:length(data_files)) # 
 {
   Data        = read.csv(paste(folder, data_files[i], sep = ''))
   X           = as.matrix(Data[, c(-1, -ncol(Data))])
   truth       = as.matrix(Data[, ncol(Data)])
-  
    Res        = GMac_bic(X, kmax = 20)
    K.hat      = Res$K
    AMI        = f_rez(Res$class, truth)$ami
-   print(c(data_files[i], AMI))
+   print(c(data_files[i], paste("AMI =", round(AMI,4)), paste("K_hat =", K.hat), paste("K_true =", length(unique(truth))))) 
+   #print(table(truth, Res$class))
 
 }
